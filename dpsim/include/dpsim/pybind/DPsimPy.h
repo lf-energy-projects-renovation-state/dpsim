a8484768eb5782d76e793e6950a8dedc0b1c4fff
Leonardo Carreras <leonardo.carreras@eonerc.rwth-aachen.de>
2026-04-23 19:50:46 +0200

    Fix pybind on windows, filesystem find and setup execution for windows wheel
    Signed-off-by: Leonardo Carreras <leonardo.carreras@eonerc.rwth-aachen.de>

---
+++ dpsim/include/dpsim/pybind/DPsimPy.h
@@ -0,0 +1,7 @@
+#include <Attributes.h>
+#include <BaseComponents.h>
+#include <DPComponents.h>
+#include <EMTComponents.h>
+#include <SPComponents.h>
+#include <SignalComponents.h>
+#include <Utils.h>
